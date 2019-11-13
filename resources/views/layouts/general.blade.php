35bd00748988f15864950f8602a00fad2381b3bc
ColeyG <cjgeerts@gmail.com>
2019-11-13 18:33:51 -0500

    Add Cold Lib | Initial Styles

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Employees MVC Site</title>
+  <link href="https://fonts.googleapis.com/css?family=Cabin%7cOld+Standard+TT&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/app.css" />
 </head>
 <body>
