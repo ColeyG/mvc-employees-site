55353c0f4df43243e4c24f69e3a15bd864debeb9
ColeyG <cjgeerts@gmail.com>
2019-11-14 00:29:11 -0500

    Base Users

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -2,4 +2,20 @@
 @section('content')
   @component('components.header')
   @endcomponent
+  <section class="employee-cards">
+    @if(App\Employee::all()->isNotEmpty())
+      @foreach(App\Employee::all() as $employee)
+        <div class="employee-card">
+          <div class="text">
+            <h3>{{ $employee->name }}</h3>
+            <p>{{ $employee->role }}</p>
+            <p>Code: {{ $employee->code }}</p>
+          </div>
+          <div class="image">
+            <img src="https://randomuser.me/api/portraits/men/0.jpg" alt="User Image">
+          </div>
+        </div>
+      @endforeach
+    @endif
+  </section>
 @endsection
