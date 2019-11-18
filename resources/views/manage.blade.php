12bc8e2c96656113be90dde2657b32011e2f4b47
ColeyG <cjgeerts@gmail.com>
2019-11-18 10:11:06 -0500

    Initial Employees Table

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -2,5 +2,25 @@
 @section('content')
   @component('components.header')
   @endcomponent
-  <h2>Manage</h2>
+  <section class="manage">
+    <h2>Manage</h2>
+    <table class="employee-table">
+      <tr>
+        <td>Name</td>
+        <td>Role</td>
+        <td>ID</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>asdf</td>
+        <td>asdf</td>
+        <td>asdf</td>
+        <td><button>More</button></td>
+        <td><button>Edit</button></td>
+        <td><button>Delete</button></td>
+      </tr>
+    </table>
+  </section>
 @endsection
