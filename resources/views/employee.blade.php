c77269b8741a46578c91346985410b5f8c7d3b16
ColeyG <cjgeerts@gmail.com>
2019-11-18 22:26:23 -0500

    Update Styles

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -2,9 +2,11 @@
 @section('content')
   @component('components.header')
   @endcomponent
-  <h2>Employee</h2>
-  <?php
-    $employee = App\Employee::find($employee);
-  ?>
-  <p>{{ $employee->name }}</p>
+  <section class="employee">
+    <h2>Employee</h2>
+    <?php
+      $employee = App\Employee::find($employee);
+    ?>
+    <p>{{ $employee->name }}</p>
+  </section>
 @endsection
