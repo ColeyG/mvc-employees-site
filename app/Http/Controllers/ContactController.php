fa378d6ba69d3b3d8a4cc8855d027f5f5bcd2fda
ColeyG <cjgeerts@gmail.com>
2019-11-26 20:58:38 -0500

    Add Contact Validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
   public function send(Request $request) {
+    $validatedData = $request->validate([
+      'name' => 'required',
+      'email' => 'required',
+      'message' => 'required'
+  ]);
     return $request->all();
   }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,10 +5,19 @@
   <section class="contact">
     <h2>Contact</h2>
     <p>Feel free to reach out with any feedback or questions!</p>
+    <ul class="error-list">
+      @foreach ($errors->all() as $error)
+        <li class="error">{{ $error }}</li>
+      @endforeach
+    </ul>
+    <p class="required">* Required</p>
     <form class="form" action="/contactMessage" method="POST">
       @csrf
+      <label for="name">Name *</label>
       <input name="name" type="text" placeholder="Name">
+      <label for="email">Email *</label>
       <input name="email" type="text" placeholder="Email">
+      <label for="message">Message *</label>
       <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
       <button type="submit">Submit</button>
     </form>
