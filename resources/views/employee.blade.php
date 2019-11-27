d23d312cb8ca7a142bed70ed875a8008cb3b6efc
ColeyG <cjgeerts@gmail.com>
2019-11-27 15:06:37 -0500

    Cleaning Up

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -13,14 +13,15 @@
       <img src="https://randomuser.me/api/portraits/{{ $employee->gender }}/{{ $employee->imageNumber }}.jpg" alt="Image of {{ $employee->name }}">
     </div>
     <p>{{ $employee->code }}</p>
-    <p>This employee is a joy to work with! They quickly updated my application, answered my questions and came up with an implementation that worked for me. Looking forward to working with them again.
+    <p>
+      This employee is a joy to work with! They quickly updated my application, answered my questions and came up with an implementation that worked for me. Looking forward to working with them again.
       <br><br>
       Fast, professional. Love this agent! I've worked with {{ $employee->name }} almost every day I've been working with them so far. He's been flexible and easy to communicate with.
       <br><br>
       Fantastic job, lovely service!! {{ $employee->name }} was professional, helpful and flexible. During the callback process I was "off the hook" and got the prompt information I needed and as quick as possible.
     </p>
     <p>
-    {{ $employee->name }}was wonderful with {{ $employee->role }}-ing. Thanks again for the thought and effort you put into doing that.
+      {{ $employee->name }} was wonderful with {{ $employee->role }}-ing. Thanks again for the thought and effort you put into doing that.
       <br><br>
       The high priced prices are like advertising for good yummy food. The service was less then adequate. The red in the beans smells up the whole restaurant. They could have covered up the stain with a slice of bread and some vinegar.
       <br><br>
